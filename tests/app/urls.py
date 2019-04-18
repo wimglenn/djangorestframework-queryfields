c1d05a4e8b6dfb8fb4e27f6cd1c7e5a1787fd414
wim glenn <wim.glenn@gmail.com>
2019-04-17 22:36:48 -0500

    test on Django 2.2 (#22)

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -1,11 +1,15 @@
+from rest_framework import __version__
 from rest_framework.routers import DefaultRouter
 
 from tests.app.views import ExplosiveViewSet
 from tests.app.views import QuoteViewSet
 from tests.app.views import SnippetViewSet
 
+v = tuple([int(x) for x in __version__.split(".")])
+basename = 'basename' if v >= (3, 9) else 'base_name'
+
 router = DefaultRouter()
-router.register(r'quotes', QuoteViewSet, base_name='api-quote')
-router.register(r'snippets', SnippetViewSet, base_name='api-snippet')
-router.register(r'explosives', ExplosiveViewSet, base_name='api-explosive')
+router.register(r'quotes', QuoteViewSet, **{basename: 'api-quote'})
+router.register(r'snippets', SnippetViewSet, **{basename: 'api-snippet'})
+router.register(r'explosives', ExplosiveViewSet, **{basename: 'api-explosive'})
 urlpatterns = router.urls
