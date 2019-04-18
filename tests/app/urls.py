86b5928586a083c61325fa4b15c18eb22ad10fb3
wim glenn <hey@wimglenn.com>
2019-04-17 21:57:32 -0500

    more tests

--- tests/app/urls.py
+++ tests/app/urls.py
@@ -1,11 +1,15 @@
+from rest_framework import __version__
 from rest_framework.routers import DefaultRouter
 
 from tests.app.views import ExplosiveViewSet
 from tests.app.views import QuoteViewSet
 from tests.app.views import SnippetViewSet
 
+vs = [int(x) for x in __version__.split(".")]
+basename = 'basename' if __version__ >= (3, 9) else 'base_name'
+
 router = DefaultRouter()
-router.register(r'quotes', QuoteViewSet, basename='api-quote')
-router.register(r'snippets', SnippetViewSet, basename='api-snippet')
-router.register(r'explosives', ExplosiveViewSet, basename='api-explosive')
+router.register(r'quotes', QuoteViewSet, **{basename: 'api-quote'})
+router.register(r'snippets', SnippetViewSet, **{basename: 'api-snippet'})
+router.register(r'explosives', ExplosiveViewSet, **{basename: 'api-explosive'})
 urlpatterns = router.urls
