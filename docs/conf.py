696ac01822451ebe17c3deab9626a613b7313006
wim glenn <hey@wimglenn.com>
2023-11-21 21:16:59 -0600

    sphinx conf

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+project = 'Django REST framework QueryFields'
+copyright = '2023, Wim Glenn'
+author = 'Wim Glenn'
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+html_static_path = ['_static']
